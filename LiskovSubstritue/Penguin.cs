acc9a18251805f560fc1fd35eced77a8a424b52d
Suresh Mekarthi <sureshmekarthi@gmail.com>
2026-01-30 11:23:15 -0500

    liskov

---
+++ LiskovSubstritue/Penguin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstritue
+{
+    internal class Penguin 
+    {
+        //public override void Fly()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public void Swim()
+        {
+            Console.WriteLine("Penguin is swimming");
+        }
+    }
+}
---
+++ LiskovSubstritue/Sparrow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstritue
+{
+    internal class Sparrow : IFlyingBird
+    {
+        public void Fly()
+        {
+            Console.WriteLine("sparrow is flying");
+        }
+    }
+}
